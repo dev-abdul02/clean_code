736c3874009fea88e85e5a11380df3d1e898b3f5
abdul <abdul@documenti.ng>
2023-07-30 11:22:22 +0530

    network test file added

--- CleanCode/CleanCodeTests/NetworkTests.swift
+++ CleanCode/CleanCodeTests/NetworkTests.swift
@@ -1,5 +1,5 @@
 //
-//  UserListTest.swift
+//  NetworkTests.swift
 //  CleanCodeTests
 //
 //  Created by abdul karim on 29/07/23.
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class UserListTest: XCTestCase {
+final class NetworkTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
